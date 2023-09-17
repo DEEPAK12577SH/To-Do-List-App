2d536ca214c0e4b3380cb5a3b659c58ea9d44bb2
Deepak Sharma <122333910+DEEPAK12577SH@users.noreply.github.com>
2023-09-17 20:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const inputBox=document.getElementById("input-box");
+const listContainer = document.getElementById("listContainer");
+
+function addTask(){
+    if(inputBox.value ===''){
+         alert(`Please fill task Name!`);
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        SaveData()
+    }
+    inputBox.value = "";
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        SaveData()
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        SaveData()
+    }
+},false)
+
+function SaveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+
+}
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+
+}
+
+showData();
+
+
+
+
+    
+   
+
